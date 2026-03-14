fe668cca4f313d04a15ebdea2f0b20e8621856e0
tju <tony.ju@me.com>
2026-03-14 09:49:09 +0800

    整个程序可以启动

--- main/main.c
+++ main/main.c
@@ -23,8 +23,8 @@ void app_main(void)
     }
     ESP_ERROR_CHECK(init_comm_channel());
     ESP_ERROR_CHECK(init_imu_sensors());
-    // ESP_ERROR_CHECK(set_device_baseline(g_user_config.device_id));
-    // ESP_ERROR_CHECK(enable_tasks());
-    // ESP_ERROR_CHECK(web_server_start());
+    ESP_ERROR_CHECK(set_device_baseline(g_user_config.device_id));
+    ESP_ERROR_CHECK(enable_tasks());
+    ESP_ERROR_CHECK(web_server_start());
 
 }
