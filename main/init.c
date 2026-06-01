eace1665ef924c5f64a16ae6aacbfaf25da8897f
tju <tony.ju@me.com>
2026-06-02 01:28:20 +0800

    clean init.c

--- main/init.c
+++ main/init.c
@@ -4,6 +4,7 @@
 #include "config_manager.h"
 #include "drv_lis2dh12.h"
 #include "drv_iis3dwb.h"
+#include "drv_t1820b.h"
 #include "logger.h"
 #include "mqtt_proxy.h"
 #include "data_dispatcher.h"
@@ -15,8 +16,8 @@
 #include "task_envelope.h"
 #include "off_sleep_manager.h"
 #include "wom_lis2dh12.h"
-#include "web_server.h"
 
+#include "esp_log.h"
 #include "esp_err.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -24,10 +25,12 @@
 #include "sdkconfig.h"
 #include <string.h>
 
-static void init_drivers()
+static void init_sensors()
 {
+    ESP_LOGD("INIT", "Initializing sensors...");
     drv_lis2dh12_init();
     drv_iis3dwb_init();
+    drv_t1820b_init();
 }
 
 static esp_err_t enable_tasks()
@@ -72,17 +75,6 @@ static esp_err_t enable_tasks()
     return ESP_OK;
 }
 
-static void network_channel_established_handler(void)
-{
-    esp_err_t err = init_mqtt_client();
-    if (err != ESP_OK)
-    {
-        LOG_ERROR("MQTT proxy initialization failed.");
-        return;
-    }
-    LOG_INFO("Network services ready.");
-}
-
 esp_err_t init_nvs()
 {
     esp_err_t ret = nvs_flash_init();
@@ -98,43 +90,11 @@ esp_err_t init_nvs()
     return ret;
 }
 
-esp_err_t establish_communication_channel()
-{
-    esp_err_t err = ESP_OK;
-    if (g_user_config.network == 1)
-    {
-        LOG_INFO("Initializing 4G Module...");
-        err = init_ppp_4g(network_channel_established_handler);
-        if (err == ESP_OK) // Ensure ppp_4g_init returns esp_err_t
-        {
-            LOG_DEBUG("communication channel established by 4G.");
-        }
-        else
-        {
-            LOG_ERROR("4G Module initialization failed.");
-        }
-    }
-    else
-    {
-        LOG_DEBUGF("ssid: %s, pass: %s", g_user_config.wifi.ssid, g_user_config.wifi.pass);
-        err = wifi_init_sta(g_user_config.wifi.ssid, g_user_config.wifi.pass, network_channel_established_handler);
-        if (err == ESP_OK)
-        {
-            LOG_DEBUG("communication channel established by WIFI.");
-        }
-        else
-        {
-            LOG_WARN("communication channel failed.");
-        }
-    }
-    return err;
-}
-
 esp_err_t start_local_services()
 {
     esp_err_t err = ESP_OK;
 
-    init_drivers();
+    init_sensors();
 
     err = data_dispatcher_start();
     if (err != ESP_OK)
@@ -154,17 +114,3 @@ esp_err_t start_local_services()
     return ESP_OK;
 }
 
-esp_err_t start_network_services()
-{
-    return establish_communication_channel();
-}
-
-void enable_config_service()
-{
-    wifi_init_softap();
-    esp_err_t ret = web_server_start();
-    if (ret != ESP_OK)
-    {
-        return;
-    }
-}
--- main/init.h
+++ main/init.h
@@ -13,9 +13,6 @@ extern "C"
     extern bool is_network_available;
     esp_err_t init_nvs();
     esp_err_t start_local_services();
-    esp_err_t start_network_services();
-    esp_err_t establish_communication_channel();
-    void enable_config_service();
 #ifdef __cplusplus
 }
 #endif
